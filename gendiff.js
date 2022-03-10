f3daa34e52c2a7a48986889fbd132d4051949f5b
Lenivin Egor <egor.lenivin@gmail.com>
2022-03-10 22:47:22 +0400

    step #2

--- gendiff.js
+++ gendiff.js
@@ -6,7 +6,10 @@ program
   .description('Compares two configuration files and shows a difference.');
 
 program
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .option('-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
   .helpOption('-h, --help', 'output usage information');
 
 program.parse();
